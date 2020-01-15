c518953a95a81704af50c40b2d976379d9d0f738
rhkdgns9489@gmail.com <rhkdgns9489@gmail.com>
2020-01-15 15:54:49 +0900

    #1.0 React App Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import Home from "./Routes/Home";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path={"/"} component={Home} exact/>
+      <Redirect to={"/"} from={"*"}/>
+    </Switch>
+  </BrowserRouter>
+);
 
 export default App;
